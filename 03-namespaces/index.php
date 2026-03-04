9bbde9dc6fbb13c9cab1d74a79e4023727d9172f
Fábio Santos <fabiosantos@ifsul.edu.br>
2026-03-04 10:23:01 -0300

    Definição de Classes e Objetos - namespaces

--- 03-namespaces/index.php
+++ 03-namespaces/index.php
@@ -4,6 +4,7 @@ require __DIR__ . "/../source/Models/User.php";
 
 use source\Models\User;
 
-$user = new User();
+$user = new User("Fábio", "fabio@gmail.com");
+$user->showUser();
 
 var_dump($user);
--- source/Models/User.php
+++ source/Models/User.php
@@ -6,9 +6,37 @@ class User
 {
     private $name;
     private $email;
-    public function __construct(string $name = null, string $email = null)
+
+    public function __construct (string $name = null, string $email = null)
     {
+        //echo "Olá, User!";
         $this->name = $name;
         $this->email = $email;
     }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+
+    public function showUser ()
+    {
+        echo "Olá, {$this->name}! E-mail: {$this->email}";
+    }
+
 }
