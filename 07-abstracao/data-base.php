51d7e2d837d1a7e4307da0d1446d58563f22f1ec
Fábio Santos <fabiosantos@ifsul.edu.br>
2026-03-31 11:19:50 -0300

    Abstração

--- 07-abstracao/data-base.php
+++ 07-abstracao/data-base.php
@@ -1,17 +1,1 @@
 <?php
-
-//use \PDO;
-
-$local = "mysql"; // localhost
-$user = "root";
-$dbName = "db-acme-manha";
-$password = "1234567"; // ""
-
-$conn = new PDO(
-    "mysql:host=$local;dbname=$dbName;charset=utf8",
-    $user,
-    $password
-);
-
-$users = $conn->query("SELECT * FROM users");
-var_dump($users->fetchAll(PDO::FETCH_OBJ));
